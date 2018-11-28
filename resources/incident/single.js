21d5f96cf0bc7c4f23bebc70270280e514771839
UsmanLanray <oakinbiyiusman@gmail.com>
2018-11-28 09:41:00 +0100

    Setup and test API endpoint that GETs a single record using Data structures

---
+++ resources/incident/single.js
@@ -0,0 +1,17 @@
+const data = require("../../data.json");
+
+module.exports = (req, res) => {
+  const incidentId = req.params.incidentId * 1;
+  const incident = data.incident.find(e => e.id === incidentId);
+  if (!incident) {
+    res.status(404).send({
+      status: 404,
+      error: "Record was not found"
+    });
+    return;
+  }
+  res.status(200).send({
+    status: 200,
+    incident
+  });
+}
