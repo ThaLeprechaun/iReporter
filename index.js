68275bb44b27691d6f47e8b1ee105f9b184ff781
UsmanLanray <oakinbiyiusman@gmail.com>
2018-11-30 23:29:00 +0100

    Change env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = require("./app");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
