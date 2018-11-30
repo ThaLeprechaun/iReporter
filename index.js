7126324be7127bea2d176263952c2f5774493add
UsmanLanray <oakinbiyiusman@gmail.com>
2018-11-30 23:52:42 +0100

    Add message to index.js file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = require("./app");
 
+app.get("/", (req, res) => res.status(200).send({ message: "Congratulations! iReporter is up and running" }));
+
 const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
